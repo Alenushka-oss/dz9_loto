88b1bf9a2f220a5d58ee5747e708a8f88584f70c
Elena <57596464+Alenushka-oss@users.noreply.github.com>
2020-04-12 14:42:21 +0300

    Add files via upload

---
+++ game.py
@@ -0,0 +1,9 @@
+from loto_classes import Game, Bag
+
+if __name__ == '__main__':
+
+    num_compics = int(input('Введите число компьютерных игроков: '))
+    num_users = int(input('Введите число живых игроков: '))
+
+    game = Game()
+    game.run(num_compics, num_users)
